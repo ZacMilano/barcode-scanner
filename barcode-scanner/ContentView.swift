8017c2d89f50b66e052a86ad47e9793a298c9382
zacmonroe <zmonroe0820@gmail.com>
2023-12-01 08:41:22 -0600

    Initial Commit

---
+++ barcode-scanner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  barcode-scanner
+//
+//  Created by Zac Milano on 12/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ barcode-scanner/barcode_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcode_scannerApp.swift
+//  barcode-scanner
+//
+//  Created by Zac Milano on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct barcode_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
